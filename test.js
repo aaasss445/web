ba51d2b11a3acd224783d89f3fd8ca3f856a6ebe
aaasss445 <45557987+aaasss445@users.noreply.github.com>
2020-02-04 06:10:07 +0900

    웹 페이지 추가, 자바스크립트 수정
    자바스크립트로 목록을 추가, 삭제, 수정할수 있게 만듬!

--- test.js
+++ test.js
@@ -1,3 +1,29 @@
+var list = {
+  "HTML 소개" : "html_introduction.html",
+  "HTML 편집기" : "html_editors.html",
+  "HTML 기본" : "html_basic.html",
+  "HTML 요소" : "html_elements.html",
+  "HTML 속성" : "html_attributes.html"
+};
+
+function listAdd(self) {
+  var filename=document.URL.substring(document.URL.lastIndexOf("/") + 1, document.URL.length);
+  for(var l in list){
+    if (list[l] == filename) {
+      $("#list").append('<li class="select"><a href=' + list[l] + '>' + l + '</a></li>');
+    }
+    else {
+        $("#list").append('<li><a href=' + list[l] + '>' + l + '</a></li>');
+    }
+  }
+}
+
+function mainListAdd(self) {
+  for(var l in list){
+    $("ul").append('<li><a href=' + list[l] + '>' + l + '</a></li>');
+  }
+}
+
 function night_day(self) {
   var target = document.querySelector('body');
   if(self.value === 'night'){
