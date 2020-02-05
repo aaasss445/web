f09b02d4e0816f415e541f45c27ab4f87e0cb4a6
aaasss445 <45557987+aaasss445@users.noreply.github.com>
2020-02-05 22:26:02 +0900

    딱히 달라진건 없음

--- test.js
+++ test.js
@@ -3,17 +3,45 @@ var list = {
   "HTML 편집기" : "html_editors.html",
   "HTML 기본" : "html_basic.html",
   "HTML 요소" : "html_elements.html",
-  "HTML 속성" : "html_attributes.html"
+  "HTML 속성" : "html_attributes.html",
+  "HTML 제목" : "html_heading.html",
+  "HTML 단락" : "html_paragraphs.html",
+  "HTML 스타일" : "html_style.html"
 };
 
+var buttonlist = [];
+var test=[];
+
+function makebuttonlist(self) {
+  var filename = document.URL.substring(document.URL.lastIndexOf("/") + 1, document.URL.length);
+  var a, s;
+  for(var i in list) {
+    buttonlist.push(i);
+  }
+  for(var i=0;i<buttonlist.length;i++){
+    if(list[buttonlist[i]] == filename){
+      a = list[buttonlist[i-1]];
+      s = list[buttonlist[i+1]];
+      if(a == undefined){
+        a = "#";
+      }
+      if(s == undefined){
+        s = "#";
+      }
+    }
+  }
+  $(".heading").append('<a href=' + a + ' class="button-l">이전</a>');
+  $(".heading").append('<a href=' + s + ' class="button-r">다음</a>');
+}
+
 function listAdd(self) {
-  var filename=document.URL.substring(document.URL.lastIndexOf("/") + 1, document.URL.length);
+  var filename = document.URL.substring(document.URL.lastIndexOf("/") + 1, document.URL.length);
   for(var l in list){
     if (list[l] == filename) {
       $("#list").append('<li class="select"><a href=' + list[l] + '>' + l + '</a></li>');
     }
     else {
-        $("#list").append('<li><a href=' + list[l] + '>' + l + '</a></li>');
+      $("#list").append('<li><a href=' + list[l] + '>' + l + '</a></li>');
     }
   }
 }
@@ -24,36 +52,38 @@ function mainListAdd(self) {
   }
 }
 
-function night_day(self) {
-  var target = document.querySelector('body');
-  if(self.value === 'night'){
-    target.style.backgroundColor='black';
-    target.style.color='white';
-    document.querySelector('#headgrid').style.borderColor='white';
-    document.querySelector('a').style.color='white';
-    self.value = 'day';
+// function night_day(self) {
+//   var target = document.querySelector('body');
+//   if(self.value === 'night'){
+//     target.style.backgroundColor='black';
+//     target.style.color='white';
+//     document.querySelector('#headgrid').style.borderColor='white';
+//     document.querySelector('a').style.color='white';
+//     self.value = 'day';
+//
+//     var links = document.querySelectorAll('a');
+//     var i =0;
+//     while(i<links.length){
+//       links[i].style.color = 'white';
+//       i = i + 1;
+//     }
+//   } else {
+//     target.style.backgroundColor='white';
+//     target.style.color='black';
+//     document.querySelector('#headgrid').style.borderColor='black';
+//     document.querySelector('a').style.color='black';
+//     self.value = 'night';
+//
+//     var links = document.querySelectorAll('a');
+//     var i =0;
+//     while(i<links.length){
+//       links[i].style.color = 'black';
+//       i = i + 1;
+//     }
+//   }
+// }
 
-    var links = document.querySelectorAll('a');
-    var i =0;
-    while(i<links.length){
-      links[i].style.color = 'white';
-      i = i + 1;
-    }
-  } else {
-    target.style.backgroundColor='white';
-    target.style.color='black';
-    document.querySelector('#headgrid').style.borderColor='black';
-    document.querySelector('a').style.color='black';
-    self.value = 'night';
 
-    var links = document.querySelectorAll('a');
-    var i =0;
-    while(i<links.length){
-      links[i].style.color = 'black';
-      i = i + 1;
-    }
-  }
-}
 // function code(code) {
 //   document.write("&lt"+code+"&gt");
 // }
